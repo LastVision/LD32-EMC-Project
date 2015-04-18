0e736bbcef72a1d332c19f70e39d8fdc688fafaa
Daniel Carlsson <daniel.carlsson92@gmail.com>
2015-04-19 01:09:05 +0200

    Added Weapon +  bullet script

---
+++ ElectromagneticCatapult/Assets/BulletDetonation.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BulletDetonation : MonoBehaviour {
+
+	// Use this for initialization
+	public float radius = 1;
+	private List<GameObject> affectedGameObjects = new List<GameObject>();
+
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void Detonation()
+	{
+		TargetObjects();
+		foreach(GameObject go in affectedGameObjects)
+		{
+			//go.GetComponents()
+		}
+	}
+
+	void TargetObjects()
+	{
+		affectedGameObjects.Clear();
+		GameObject[] allGameObjects;
+		allGameObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+		foreach(GameObject go in allGameObjects)
+		{
+			if(Vector3.Distance(this.transform.position, go.transform.position) < radius && go.activeInHierarchy)
+			{
+				affectedGameObjects.Add(go);
+			}
+		}
+	}
+}
