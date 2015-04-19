0322d3d1b7ba6972c17ff7a3c3aa61efb5c7fc81
Daniel Carlsson <daniel.carlsson92@gmail.com>
2015-04-19 02:26:50 +0200

    Added PlayerExplodingBullet

--- ElectromagneticCatapult/Assets/PlayerWeapon.cs
+++ ElectromagneticCatapult/Assets/PlayerWeapon.cs
@@ -1,41 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerWeapon : MonoBehaviour 
+public class PlayerWeapon : MonoBehaviour
 {
 	public GameObject explosionBullet;
 	public GameObject implosionBullet;
-
 	public float Speed = 1;
-
-	private GameObject currentExpBullet;
-	private GameObject currentImpBullet;
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void Update ()
 	{
-		if(Input.GetKeyUp(KeyCode.Space) == true)
-		{
-			if(currentExpBullet)
-			{
-				BulletDetonation bullet = currentExpBullet.GetComponent<BulletDetonation>();
-				bullet.Detonation();
+		if (Input.GetButtonDown("Fire1") == true) {
+			Instantiate (explosionBullet, transform.position, transform.rotation);
+		}
+		else if (Input.GetButtonUp("Fire1") == true) {
+			GameObject bulletMove = GameObject.FindGameObjectWithTag ("ExplosionBullet");
+			if (bulletMove) {
+				bulletMove.GetComponent<PlayerBulletMovement> ().SetBulletStop(true);
+				bulletMove.GetComponent<BulletDetonation> ().Detonation();
 			}
 		}
-		if(Input.GetKeyDown(KeyCode.Space) == true)
-		{
-			currentExpBullet = Instantiate(explosionBullet);
-			currentExpBullet.transform.position = transform.position;
-			currentExpBullet.transform.rotation = transform.rotation;
-		}
-		if(Input.GetKey(KeyCode.Space) == true)
-		{
-			currentExpBullet.transform.position += Input.mousePosition * Speed;
+		else if (Input.GetButton("Fire1") == true) {
+			GameObject bulletMove = GameObject.FindGameObjectWithTag ("ExplosionBullet");
+			if (bulletMove) {
+				bulletMove.GetComponent<PlayerBulletMovement> ().SetBulletStop(false);
+			}
 		}
 	}
 }
